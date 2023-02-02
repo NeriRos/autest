afeacbd14c4e722d86860b09f41b3c59e41aa4b2
Neriya Rosner <neriyarosner@gmail.com>
2023-02-02 18:17:36 +0200

    design

--- client/components/demo.js
+++ client/components/demo.js
@@ -1,9 +1,41 @@
+import { InboxOutlined } from '@ant-design/icons';
+import { Upload } from 'antd'
+const { Dragger } = Upload;
 
 const Demo = () => {
+  const props = {
+    name: 'file',
+    multiple: true,
+    action: 'http://test-writer-srv:3000/api/test-writer',
+    onChange(info) {
+      const { status } = info.file;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+    onDrop(e) {
+      console.log('Dropped files', e.dataTransfer.files);
+    },
+  };
+
   return (
-    <div>
-      <h1>DEMOOO</h1>
-    </div>
+    <>
+      <h1>Component to Test</h1>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">Click or drag component file to this area to upload</p>
+        <p className="ant-upload-hint">
+          Upload a react js component and receive its test
+        </p>
+      </Dragger>
+    </>
   );
 };
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,10 @@
 import Demo from "../components/demo";
 
 const Index = () => {
-  return <div>
+  return <div style={{
+    padding: "3em",
+    maxWidth: 1240,
+  }}>
     <Demo />
   </div>
 };
