f186657804a24421bbec4cfc080a1299ebee38dc
Neriya Rosner <neriyarosner@gmail.com>
2023-02-12 15:35:17 +0200

    Updates

--- common/build/src/events/subjects.js
+++ common/build/src/events/subjects.js
@@ -1,7 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Subjects = void 0;
-var Subjects;
+export var Subjects;
 (function (Subjects) {
     Subjects["FileUpload"] = "file_upload";
-})(Subjects = exports.Subjects || (exports.Subjects = {}));
+})(Subjects || (Subjects = {}));
